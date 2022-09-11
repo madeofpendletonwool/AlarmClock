48d898832643b4c44fdfe41e811c16cbda8ae2a7
brickinthewall10 <blacanwite@gmail.com>
2022-09-11 15:39:59 -0500

    Removing old Files and changing alarm save file to alarms.yaml

--- soundtest.py
+++
@@ -1,2 +0,0 @@
-exec(open("/home/collinp/Documents/GitHub/AlarmClock/sound1.py").read())
-print("This comes after")
--- timing.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def now():
-    ntime=datetime.datetime.now()
-    nt=ntime.strftime('%H:%M:%S')
-    return nt
-
-now()
