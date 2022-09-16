72df55d03cfbe872c956f77ad65837a247ab1cd5
brickinthewall10 <blacanwite@gmail.com>
2022-09-15 22:06:46 -0500

    Adding Lots of little inprovments - snooze setting default - clock now stays open when alarm is set - moved alarm options to separate page - removed some garbage code

--- blob.py
+++ blob.py
@@ -2,9 +2,9 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 print(voices)
-# for voice in voices:
-#     print(voice, voice.id)
-#     engine.setProperty('voice', voice.id)
-#     engine.say("Hello World!")
-#     engine.runAndWait()
-#     engine.stop()
+for voice in voices:
+    print(voice, voice.id)
+    engine.setProperty('voice', voice.id)
+    engine.say("Hello world")
+    engine.runAndWait()
+    engine.stop()
